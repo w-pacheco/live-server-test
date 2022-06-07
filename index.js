7e1c5dc08b187e08d5579519b87d0c9c0a3599b1
w-pacheco <wilfredo.pacheco.saic.com>
2022-06-07 13:34:34 -0500

    added message variable

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 
     async function init(){
 
+        const Message = 'Hello World!!!';
         const HelloElement = document.createElement('div');
-        HelloElement.innerHTML = /*html*/`
-        <h1>Hello World!</h1>`;
-
+        HelloElement.innerHTML = /*html*/`<h1>${Message}</h1>`;
         document.body.append(HelloElement);
-
     }
 
     window.onload = init;
